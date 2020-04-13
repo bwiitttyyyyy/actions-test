85d0f4c8d2c5e9228ea979fb8822a76a5e66b28b
bwiitttyyyyy <brandon.d.witt@protonmail.com>
2020-04-13 11:39:02 -0700

    chore: test

--- .github/scripts/install-aws-cli.sh
+++ .github/scripts/install-aws-cli.sh
@@ -1,7 +1,11 @@
 echo "Insalling AWS CLI..."
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 sudo ./aws/install
+
+echo "Verifying..."
+
+/usr/local/bin/aws --version
 
 echo "Done."
