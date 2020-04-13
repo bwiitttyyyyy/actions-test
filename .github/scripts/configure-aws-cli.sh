1f4b9561512c8ae13ff3b8115af011df0151cb8a
bwiitttyyyyy <brandon.d.witt@protonmail.com>
2020-04-13 12:12:42 -0700

    chore: test

--- .github/scripts/configure-aws-cli.sh
+++ .github/scripts/configure-aws-cli.sh
@@ -19,4 +19,8 @@ echo "[staging]" >> ~/.aws/credentials
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID_STAGING" >> ~/.aws/credentials
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY_STAGING" >> ~/.aws/credentials
 
+echo "Verifying..."
+
+aws configure list
+
 echo "Done."
