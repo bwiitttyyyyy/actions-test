e8a67f5a59aee57659b7aaa32aba154323264131
bwiitttyyyyy <brandon.d.witt@protonmail.com>
2020-04-13 12:09:41 -0700

    chore: test

--- .github/scripts/configure-aws-cli.sh
+++ .github/scripts/configure-aws-cli.sh
@@ -1,5 +1,11 @@
 echo "Configuring AWS CLI..."
 
+# verify that all of the necessary environment variables are set
+[ -z "$AWS_ACCESS_KEY_ID_PRODUCTION" ] && echo "AWS_ACCESS_KEY_ID_PRODUCTION not set" && exit 1
+[ -z "$AWS_SECRET_ACCESS_KEY_PRODUCTION" ] && echo "AWS_SECRET_ACCESS_KEY_PRODUCTION not set" && exit 1
+[ -z "$AWS_ACCESS_KEY_ID_STAGING" ] && echo "AWS_ACCESS_KEY_ID_STAGING not set" && exit 1
+[ -z "$AWS_ACCESS_KEY_ID_STAGING" ] && echo "AWS_SECRET_ACCESS_KEY_STAGING  not set" && exit 1
+
 mkdir ~/.aws
 cp ./.github/resources/aws/* ~/.aws/
 
