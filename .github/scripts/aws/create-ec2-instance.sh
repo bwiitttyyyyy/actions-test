dad8a37c13823f1744f7d4ccc2f8e03783abe2cd
bwiitttyyyyy <brandon.d.witt@protonmail.com>
2020-04-13 20:56:12 -0700

    chore: test

--- .github/scripts/aws/create-ec2-instance.sh
+++ .github/scripts/aws/create-ec2-instance.sh
@@ -81,6 +81,6 @@ echo "Created Elastic IP address $IP_ADDRESS"
 # ssh into the instance
 echo "Entering instance..."
 echo "$AWS_SSH_KEY" >> aws_ssh_key
-ssh -T -i aws_ssh_key centos@$IP_ADDRESS
+cat ../centos/os-setup.sh | ssh -T -i aws_ssh_key centos@$IP_ADDRESS
 
 echo "Done."
---
+++ .github/scripts/centos/os-setup.sh
@@ -0,0 +1,3 @@
+hostname
+echo "hello world"
+cwd
