4b634c241f099c21d0da9d354666bbea2690291d
bwiitttyyyyy <brandon.d.witt@protonmail.com>
2020-04-13 19:10:58 -0700

    chore: test

--- .github/scripts/aws/create-ec2-instance.sh
+++ .github/scripts/aws/create-ec2-instance.sh
@@ -1,7 +1,36 @@
+#!/bin/bash
+
 echo "Creating EC2 instance..."
 
-# aws ec2 run-instances --image-id ami-173d747e --count 1 --instance-type t1.micro --key-name MyKeyPair --security-groups my-sg
-#aws ec2 run-instances --count 1 --instance-type t1.micro --key-name MyKeyPair --security-groups my-sg
+# find deployment user key pair ID
+#aws iam list-users >> users.yml
+#N_USERS=$(yq r users.yml --collect --length Users.*.UserName)
+#
+#for (( USER_INDEX = 0; USER_INDEX <= $N_USERS; USER_INDEX++ ));
+#do
+#  USERNAME=$(yq r users.yml Users.[$USER_INDEX].UserName)
+#  USER_ID=$(yq r users.yml Users.[$USER_INDEX].UserId)
+#  if [ "$USERNAME" == $AWS_DEPLOYMENT_USERNAME ]
+#    
+#  fi
+#done
 
+#AWS_DEPLOYMENT_USERNAME=deployment@madhattertech.ca
+#GITHUB_SHA=12345
+#GITHUB_REPOSITORY=dontworrru
 
+# create the key pair from SSH key 
+echo "Creating key pair..."
+AWS_DEPLOYMENT_PUBLIC_KEY_ID=$(aws iam list-ssh-public-keys --user-name $AWS_DEPLOYMENT_USERNAME | yq r - SSHPublicKeys.[0].SSHPublicKeyId)
+aws iam get-ssh-public-key --user-name $AWS_DEPLOYMENT_USERNAME --ssh-public-key-id $AWS_DEPLOYMENT_PUBLIC_KEY_ID --encoding SSH \
+  | yq r - SSHPublicKey.SSHPublicKeyBody >> ssh.pub
+echo "WARNING: this may complain about a duplicate key ... this is not an issue"
+aws ec2 import-key-pair --key-name $GITHUB_SHA --public-key-material fileb://$PWD/ssh.pub
+
+# create the instance
+echo "Creating instance..."
+SUBNET_ID=$(yq r subnet.yml Subnet.SubnetId)
+aws ec2 run-instances --image-id ami-04a25c39dc7a8aebb --count 1 --instance-type t2.micro --key-name $GITHUB_SHA --subnet $SUBNET_ID >> instance.yml
+INSTANCE_ID=$(yq r instance.yml Instances.[0].InstanceId)
+aws ec2 create-tags --resources $ROUTE_TABLE_ID --tags Key=commit,Value=$GITHUB_SHA Key=repository,Value=$GITHUB_REPOSITORY --profile production
 echo "Done."
--- .github/scripts/aws/create-network.sh
+++ .github/scripts/aws/create-network.sh
@@ -6,21 +6,21 @@ echo "Creating Network..."
 echo "Creating VPC..."
 aws ec2 create-vpc --cidr-block 10.0.0.0/16 --profile production >> vpc.yml
 VPC_ID=$(yq r vpc.yml Vpc.VpcId)
-aws ec2 create-tags --resources $VPC_ID --tags Key=commit,Value=$GITHUB_SHA --profile production
+aws ec2 create-tags --resources $VPC_ID --tags Key=commit,Value=$GITHUB_SHA Key=repository,Value=$GITHUB_REPOSITORY --profile production
 echo "Created VPC with ID $VPC_ID"
 
 # create subnet
 echo "Creating subnet..."
 aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block 10.0.0.0/16 --profile production >> subnet.yml
 SUBNET_ID=$(yq r subnet.yml Subnet.SubnetId)
-aws ec2 create-tags --resources $SUBNET_ID --tags Key=commit,Value=$GITHUB_SHA --profile production
+aws ec2 create-tags --resources $SUBNET_ID --tags Key=commit,Value=$GITHUB_SHA Key=repository,Value=$GITHUB_REPOSITORY --profile production
 echo "Created subnet with ID $SUBNET_ID"
 
 # create internet gateway
 echo "Creating internet gateway..."
 aws ec2 create-internet-gateway --profile production >> gateway.yml
 GATEWAY_ID=$(yq r gateway.yml InternetGateway.InternetGatewayId)
-aws ec2 create-tags --resources $GATEWAY_ID --tags Key=commit,Value=$GITHUB_SHA --profile production
+aws ec2 create-tags --resources $GATEWAY_ID --tags Key=commit,Value=$GITHUB_SHA Key=repository,Value=$GITHUB_REPOSITORY--profile production
 echo "Created internet gateway with ID $GATEWAY_ID"
 
 # attach gateway to VPC
@@ -32,7 +32,7 @@ echo "Attached gateway"
 echo "Creating custom route table..."
 aws ec2 create-route-table --vpc-id $VPC_ID --profile production >> route_table.yml
 ROUTE_TABLE_ID=$(yq r route_table.yml RouteTable.RouteTableId)
-aws ec2 create-tags --resources $ROUTE_TABLE_ID --tags Key=commit,Value=$GITHUB_SHA --profile production
+aws ec2 create-tags --resources $ROUTE_TABLE_ID --tags Key=commit,Value=$GITHUB_SHA Key=repository,Value=$GITHUB_REPOSITORY --profile production
 echo "Creating route to public internet..."
 aws ec2 create-route --route-table-id $ROUTE_TABLE_ID --destination-cidr-block 0.0.0.0/0 --gateway-id $GATEWAY_ID --profile production
 echo "Associating route table with subnet (making public)..."
