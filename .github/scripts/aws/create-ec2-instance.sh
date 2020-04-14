92b0bf4bded9868ff08eaf409b66696b96a0e329
bwiitttyyyyy <brandon.d.witt@protonmail.com>
2020-04-13 22:14:49 -0700

    chore: test

--- .github/scripts/aws/create-ec2-instance.sh
+++ .github/scripts/aws/create-ec2-instance.sh
@@ -97,6 +97,7 @@ done
 # ssh into the instance
 echo "Entering instance..."
 echo "$AWS_SSH_KEY" >> aws_ssh_key
+sudo chmod 640 aws_ssh_key
 ssh -A -T -o StrictHostKeyChecking=no -i aws_ssh_key root@$IP_ADDRESS "hostname" > fill
 cat fill
 
